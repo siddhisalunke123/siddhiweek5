58801e1c3fb2168ac56e1361b4bc12a5d3c9d7b1
Iamneo <techadmin@iamneo.ai>
2023-11-20 12:55:09 +0000

    Backup Commit

--- angularapp/src/app/model/iteam.ts
+++ angularapp/src/app/model/iteam.ts
@@ -1,3 +1,4 @@
 export interface ITeam {
-    
+    teamid : number
+    teamname : string
 }
---
+++ angularapp/src/app/services/playerservice.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerserviceService {
+
+  constructor() { }
+
+}
