caaca55fe95f6e0aaf97058273dc6292f2733043
Iamneo <techadmin@iamneo.ai>
2023-11-21 04:57:56 +0000

    Backup Commit

--- angularapp/src/app/services/admin.service.ts
+++ angularapp/src/app/services/admin.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { Player } from 'src/models/player.model';
+import { Team } from 'src/models/team.model';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
   private url='https://8080-abafbabeebcfacbbecabcdadeafbbdcaeafe.premiumproject.examly.io/Admin/'
+  private url1 = 'https://8080-abafbabeebcfacbbecabcdadeafbbdcaeafe.premiumproject.examly.io/Admin'
 
   constructor(private httpclient:HttpClient) { }
   getPlayers():Observable<any[]>
   {
     return this.httpclient.get<any[]>(this.url+'/GetPlayer')
+  }
+  getTeams():Observable<any[]>
+  {
+    return this.httpclient.get<any[]>(this.url1+'/GetTeams')
   }
 }
--- angularapp/src/model/team.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Team } from './team.model';
-
-describe('Team', () => {
-  it('should create an instance', () => {
-    expect(new Team()).toBeTruthy();
-  });
-});
