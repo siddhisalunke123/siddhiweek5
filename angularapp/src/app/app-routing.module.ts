d5a30f68ba3c57df5156279dad3794ec834e26f4
Iamneo <techadmin@iamneo.ai>
2023-11-22 15:44:43 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-
-const routes: Routes = [
-  { path: 'admin', component: AdminComponent }
+import { HomeComponent } from './home/home.component';
+import { EditPlayerComponent } from './edit-player/edit-player.component';
+import { AddTeamsComponent } from './add-teams/add-teams.component';
+import { AddPlayersComponent } from './add-players/add-players.component';
+import { ListPlayersComponent } from './list-players/list-players.component';
+import { FormGroup } from '@angular/forms';
+import { DeleteplayerComponent } from './deleteplayer/deleteplayer.component';
+import { ListTeamsComponent } from './list-teams/list-teams.component';
+import { DeleteTeamComponent } from './delete-team/delete-team.component';
+const routes: Routes = [ 
+  {path:"",component:HomeComponent},
+  {path:"addteam",component:AddTeamsComponent},
+  {path:"addplayer",component:AddPlayersComponent},
+  {path:"edit/:id",component:EditPlayerComponent},
+  {path:"listplayer",component:ListPlayersComponent},
+  {path:"delete/:id",component:DeleteplayerComponent},
+  {path: "listeams",component:ListTeamsComponent},
+  {path:"del/:id",component:DeleteTeamComponent}
+//edit/:id 
+//delete/:id
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
- 
-}
+export class AppRoutingModule { }
