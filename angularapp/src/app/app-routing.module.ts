e51f74692c3358d60b094b0c0b2c32e76c30d595
Iamneo <techadmin@iamneo.ai>
2023-11-23 11:58:25 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AddTeamComponent } from './add-team/add-team.component';
-import { AddPlayersComponent } from './add-players/add-players.component';
-import { HomeComponent } from './home/home.component';
-import { EditPlayerComponent } from './edit-player/edit-player.component';
-import { ListPlayersComponent } from './list-players/list-players.component';
-import { DeletePlayerComponent } from './delete-player/delete-player.component';
 
 const routes: Routes = [
-  {path:"home",component:HomeComponent},
-  {path:"addteam",component:AddTeamComponent},
-  {path:"addplayer",component:AddPlayersComponent},
-  {path:"editplayer/:id",component:EditPlayerComponent},
-  {path:"listplayer",component:ListPlayersComponent},
-  {path:"delete/:id", component:DeletePlayerComponent}
-
 ];
 
 @NgModule({
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,36 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListPlayersComponent } from './list-players/list-players.component';
-import { ListTeamComponent } from './list-team/list-team.component';
-import { AddPlayersComponent } from './add-players/add-players.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { PlayerComponent } from './player/player.component';
-// import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { EditPlayerComponent } from './edit-player/edit-player.component';
-import { MenuComponent } from './menu/menu.component';
 import { FormsModule } from '@angular/forms';
-import { DeletePlayerComponent } from './delete-player/delete-player.component';
-import { AddTeamComponent } from './add-team/add-team.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListPlayersComponent,
-    ListTeamComponent,
-    AddPlayersComponent,
-    PlayerComponent,
-    HomeComponent,
-  
-    EditPlayerComponent,
-    MenuComponent,
-   
-    DeletePlayerComponent,
-   
-    AddTeamComponent,
-   
-    ListTeamComponent
+    
   ],
   imports: [
     BrowserModule,
